3c205a955f0319dda689fd5b1d2a826fdf1a4956
IsaacGH <garciaisaac468@gmail.com>
2019-11-02 11:32:19 -0600

    add image background in login and link to register

--- app/src/main/java/com/example/mychat/Login.java
+++ app/src/main/java/com/example/mychat/Login.java
@@ -2,13 +2,29 @@ package com.example.mychat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    private TextView toRegister;
+    private Intent intentRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        toRegister = findViewById(R.id.toRegister);
+        intentRegister = new Intent(this, Register.class);
+        toRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(intentRegister);
+                finish();
+            }
+        });
     }
 }
