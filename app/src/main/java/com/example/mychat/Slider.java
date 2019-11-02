31456cadee8fa367374ff2f7b8c55f1eeb1e8195
IsaacGH <garciaisaac468@gmail.com>
2019-11-02 15:11:46 -0600

    add text's, buttons and link's

--- app/src/main/java/com/example/mychat/Slider.java
+++ app/src/main/java/com/example/mychat/Slider.java
@@ -2,13 +2,42 @@ package com.example.mychat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Slider extends AppCompatActivity {
+
+    private Button goToRegisterSlider;
+    private TextView goToLoginSlider;
+    private Intent register, login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slider);
+
+        goToRegisterSlider = findViewById(R.id.goToRegisterSlider);
+        goToLoginSlider = findViewById(R.id.goToLoginSlider);
+        register = new Intent(this, Register.class);
+        login = new Intent(this, Login.class);
+
+        goToRegisterSlider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(register);
+                finish();
+            }
+        });
+
+        goToLoginSlider.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(login);
+                finish();
+            }
+        });
     }
 }
